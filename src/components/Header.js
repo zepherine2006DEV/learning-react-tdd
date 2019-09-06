4e62aa2e4ece448cedda54ddf75d530d7c443766
James Hylins <james.hylins@johnlewis.co.uk>
2019-09-06 13:44:29 +0100

    refactor: organise directory structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Component } from 'react';
 
+// stateless component
+
 export class Header extends Component {
   render() {
     return (
-      <div className="App"></div>
+      <div className="Header">
+        <p>this is the header</p>
+      </div>
     );
   }
 }
